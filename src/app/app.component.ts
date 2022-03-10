841e222da047560c94745568faa4315a50598d06
AndreaStucchi03 <stucchi.andrea@iisgalvanimi.edu.it>
2022-03-10 09:07:11 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular-Empty-Project';
+
+
+export class AppComponent{
+  testo_casella:string=""
+  testo_salvato:string="(ancora nulla)"
+  testo_colore:string=""
+  testo_porte:string=""
+
+  constructor() {}
+
+  onClick() {
+    this.testo_salvato=this.testo_casella+" " +" "+this.testo_colore+" "+this.testo_porte
+
+  }
+
 }
